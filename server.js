19e1e56c37d61fddad8201fc58f11070b1a6f93d
Kiervy Estole <kiervyxestole@gmail.com>
2025-10-28 18:28:38 +0800

    modified

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { pool, createUsersTable } = require('./db');
 const bcrypt = require('bcrypt');
-
+//  
 const app = express();
 const PORT = process.env.PORT || 5000;
 
